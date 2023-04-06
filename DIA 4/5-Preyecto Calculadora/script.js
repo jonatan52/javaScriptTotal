ed6b521f283942b75745c4f3ac4718239ccce169
Jonatan <tarriba52@gmail.com>
2023-04-05 21:43:22 -0500

    creando funciones

---
+++ DIA 4/5-Preyecto Calculadora/script.js
@@ -0,0 +1,31 @@
+//Funcion suma
+function suma(num1, num2){
+    let resultadoSuma = num1 + num2;
+    return resultadoSuma;
+}
+
+//Funcion resta
+function resta(){
+    let resultadoResta = num1 - num2;
+    return resultadoResta;
+}
+
+//Funcion multiplicacion
+function multiplicacion(){
+    let resultadoMultiplicar = num1 * num2;
+    return resultadoMultiplicar;
+}
+
+//Funcion division
+function dividir(){
+    let resultadoMultiplicar = num1 / num2;
+    return resultadoDivision;
+}
+
+//Funcion raiz cuadrada
+function raizCuadrada(num){
+    let raiz = Math.sqrt(num);
+    return raiz;
+}
+console.log(raizCuadrada(5))
+
