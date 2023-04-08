9f7bde88d25118c331c259b82d082f754b3b12b8
Jonatan <tarriba52@gmail.com>
2023-04-08 18:40:28 -0500

    practica calculadora

--- DIA 4/5-Preyecto Calculadora/script.js
+++ DIA 4/5-Preyecto Calculadora/script.js
@@ -1,31 +1,71 @@
+//Funcion para mostrar resultado
+function mostrarResultado(resultado){
+    document.getElementById("resultado").value = resultado;
+}
+
+
 //Funcion suma
-function suma(num1, num2){
-    let resultadoSuma = num1 + num2;
-    return resultadoSuma;
+function suma(){
+    let num1 = +document.getElementById("numUno").value;
+    let num2 = +document.getElementById("numDos").value;
+    
+    mostrarResultado(num1 + num2);
 }
 
 //Funcion resta
 function resta(){
-    let resultadoResta = num1 - num2;
-    return resultadoResta;
+    let num1 = +document.getElementById("numUno").value;
+    let num2 = +document.getElementById("numDos").value;    
+    mostrarResultado(num1 - num2);
 }
 
 //Funcion multiplicacion
 function multiplicacion(){
-    let resultadoMultiplicar = num1 * num2;
-    return resultadoMultiplicar;
+    let num1 = +document.getElementById("numUno").value;
+    let num2 = +document.getElementById("numDos").value;    
+    mostrarResultado(num1 * num2);
 }
 
 //Funcion division
 function dividir(){
-    let resultadoMultiplicar = num1 / num2;
-    return resultadoDivision;
+    let num1 = +document.getElementById("numUno").value;
+    let num2 = +document.getElementById("numDos").value;    
+    mostrarResultado(num1 / num2);
 }
 
 //Funcion raiz cuadrada
 function raizCuadrada(num){
-    let raiz = Math.sqrt(num);
+    let raiz = Math.sqrt(num) 
     return raiz;
 }
-console.log(raizCuadrada(5))
 
+//Funcion valor absoluto
+function valorAbsoluto(num){
+    let absoluto = Math.abs(num);
+    return absoluto;
+}
+
+//Funcion redondeo Abajo
+function redondeoAbajo(){
+    let redondearAbajo = Math.floor();
+
+    return redondearAbajo;
+}
+
+//Funcion redondeo Arriba
+function redondeoArriba(){
+    let redondearArriba = Math.ceil();
+
+    return redondearArriba;
+}
+
+
+//Funcion un numero aleatorio
+function elementoRandon(){
+    let randon = Math.random();
+}
+
+//Funcion para redondear 
+function redondear(){
+    let randon = Math.round();
+}
