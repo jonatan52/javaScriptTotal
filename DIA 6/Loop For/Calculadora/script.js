f5605538cbbab55aac159751ff8dbfc719502580
Jonatan <tarriba52@gmail.com>
2023-10-09 20:08:08 -0500

    Practica for

---
+++ DIA 6/Loop For/Calculadora/script.js
@@ -0,0 +1,28 @@
+function multiplicar(){
+    //Obtener numero
+    let elementoTabla = document.getElementById("textoTabla");
+    let textoNumeroTabla = elementoTabla.value;
+    let numeroTabla = Number(textoNumeroTabla);
+
+    // Obtener la tabla
+    let elementoTablaMultiplicar = document.getElementById("listaTabla");
+
+    //Producir y mostrar resultados
+    for(i=1; i<=10; i++){
+
+        //Calcular el resultado
+        let numeroResultado = numeroTabla * i;
+
+        // Armar string con resultado
+        let textoResultado = numeroTabla + " X " + i + " = " + numeroResultado;
+
+        //Crear un elemento de la lsita
+        let itemLista = document.createElement("li");
+        itemLista.innerText = textoResultado;
+        elementoTablaMultiplicar.appendChild(itemLista); 
+       
+
+    }
+    
+
+}
--- DIA 6/Loop For/Ejemplo For/script.js
+++
@@ -1,4 +0,0 @@
-for(x = 0; x < 5; x++){
-    document.write("<h"+ x + ">Hola Mundo</h" + x + ">");
-    
-}
