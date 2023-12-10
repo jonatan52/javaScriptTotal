cd838b13d57e5c72f5a5c4be6687d663baac75fa
Jonatan <tarriba52@gmail.com>
2023-12-09 20:37:24 -0500

    creando funciones

--- DIA 6/Proyecto Dia 6/index.js
+++ DIA 6/Proyecto Dia 6/index.js
@@ -1,34 +1,52 @@
-let arrayNota = [4.5, 5.2, 3.5,3.2]
-for(let nota of arrayNota){
+// let arrayNota = [4.5, 5.2, 3.5,3.2]
+// for(let nota of arrayNota){
+//    // let suma = nota+=nota;
+//   console.log(nota)
     
-}
+// }
+
+
 
 function promedio(){
-   let nota1 =  document.querySelector("#nota1")
-   nota1.value=5;
+   let nota1 = parseInt(document.querySelector("#nota1").value)
+   console.log(nota1)
+   
+   let nota2 = parseInt(document.querySelector("#nota2").value)
 
-   let nota2 =  document.querySelector("#nota2")
-   nota2.value=5;
+   let nota3 = parseInt(document.querySelector("#nota3").value)
 
-   let nota3 =  document.querySelector("#nota3")
-   nota3.value=5;
-   console.log(nota3)
+   let nota4 = parseInt(document.querySelector("#nota4").value)
 
-   let nota4 =  document.querySelector("#nota4")
-   nota4.value=5;
+   let promedioNota = parseInt((nota1 + nota2 + nota3 + nota4)/4);
+   console.log(promedioNota)
 
-   let suma = parseInt((nota1 + nota2 + nota3 + nota4)/4);
-
-    let prom = document.querySelector("#pro").innerHTML=`hola`;
-   return  parseInt(prom);
+    let prom = document.querySelector("#promedio").innerHTML=`<span>${parseInt(promedioNota)}</span>`
+   return  prom;
 
 
 }
 
 function NotaAlta(){
+   let nota1 = parseInt(document.querySelector("#nota1").value)
+   
+   
+   let nota2 = parseInt(document.querySelector("#nota2").value)
 
+   let nota3 = parseInt(document.querySelector("#nota3").value)
+
+   let nota4 = parseInt(document.querySelector("#nota4").value)
+
+   let notaMax = Math.max(nota1, nota2, nota3, nota4);
+   
+  document.querySelector("#NotaAlta").innerHTML=`<span>${parseInt(notaMax)}</span>`
+  
 }
 
 function aplazos(){
-
+   promedio()
+   if(promedio.promedioNota < 4){
+      document.querySelector("#Aplazos").innerHTML=`<span>Aplazos</span>`
+   }
+  
+   
 }
