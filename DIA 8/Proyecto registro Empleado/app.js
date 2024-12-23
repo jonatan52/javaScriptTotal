3ef8afda76551bb40ee967f5b252969cb130d00d
jonatan52 <tarriba52@gmail.com>
2024-12-22 22:07:37 -0500

    fromu

---
+++ DIA 8/Proyecto registro Empleado/app.js
@@ -0,0 +1,4 @@
+let legajo = document.querySelector('#apellido').value;
+let nombre = document.querySelector('nombre').value;
+let apellid =document.querySelector('#apellido').value
+let legajo = document.querySelector('#apellido').value
