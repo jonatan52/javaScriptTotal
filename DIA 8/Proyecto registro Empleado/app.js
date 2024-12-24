2f842156316b2fab062bffb099a9df2dc59fbc72
jonatan52 <tarriba52@gmail.com>
2024-12-24 11:31:35 -0500

    Practica dia 8 terminada

--- DIA 8/Proyecto registro Empleado/app.js
+++ DIA 8/Proyecto registro Empleado/app.js
@@ -1,10 +1,9 @@
 let botonCarga = document.querySelector('#cargarEmpleado');
 let botonMostrarEmpleado = document.querySelector('#mostrarEmpleado');
-
 let empleados = [];
 
 class Empleado {
-    constructor(legajo, nombre, fecha, cargo) {
+    constructor(legajo, nombre, apellido, fecha, cargo) {
         this.legajo = legajo;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -13,32 +12,36 @@ class Empleado {
 
     }
 }
-
+//Funcion para crear un empleado
 function crearEmpleado() {
     let legajoUsario = document.querySelector('#legajo').value;
     let nombreUsuario = document.querySelector('#nombre').value;
-    let apllidoeUsuario = document.querySelector('#apellido').value;
+    let apellidoeUsuario = document.querySelector('#apellido').value;
     let fechaUsuario = document.querySelector('#fecha').value;
     let cargoUsuario = document.querySelector('#cargo').value;
 
-
-    let empleado1 = new Empleado(legajo, nombre, apellido, fecha, cargo);
-    empleado1.legajo = legajoUsario;
-    empleado1.nombre = nombreUsuario;
-    empleado1.apellido = apllidoeUsuario;
-    empleado1.fecha = fechaUsuario;
-    empleado1.cargo = cargoUsuario;
+    let empleado1 = new Empleado(legajoUsario, nombreUsuario, apellidoeUsuario, fechaUsuario, cargoUsuario);
     empleados.push(empleado1);
     alert('El empelado ha sido cargado con exito')
-
-    console.log(empleados);
-    function mostrarEmpleado() {
-        for (let emp in empleados) {
-            alert(`${emp}  Codigo:${empleado1.legajo} Nombre:${empleado1.nombre} Fecha:${empleado1.fecha} Cargo:${empleado1.cargo}`)
-        }
-    }
-    
-    botonMostrarEmpleado.addEventListener('click', mostrarEmpleado)
-   
+    limpiarInput();
 }
-botonCarga.addEventListener('click', crearEmpleado);
+
+//Funcion para mostrar un empleado
+function mostrarEmpleado() {
+    let empleadosInfo = empleados.map(emp =>
+        `Legajo: ${emp.legajo}, Nombre: ${emp.nombre}, Apellido: ${emp.apellido}, Fecha: ${emp.fecha}, Cargo: ${emp.cargo}`
+    ).join('\n');
+    alert(empleadosInfo);
+
+}
+
+//Funcion para limpiar los input
+function limpiarInput() {
+    document.querySelector('#legajo').value = '';
+    document.querySelector('#nombre').value = '';
+    document.querySelector('#apellido').value = '';
+    document.querySelector('#fecha').value = '';
+    document.querySelector('#cargo').value = '';
+}
+botonMostrarEmpleado.addEventListener('click', mostrarEmpleado);
+botonCarga.addEventListener('click', crearEmpleado)
